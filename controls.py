8de707d063b5092c182ff225a32a332cc4c2c50b
wemomok <64313110+wemomok@users.noreply.github.com>
2022-04-18 21:44:20 +0300

    last for 18.04

--- controls.py
+++ controls.py
@@ -2,4 +2,7 @@ import pygame as pg
 from sys import *
 
 def maincontrol():
-    pass
+    for e in pg.event.get():
+        if e.type == pg.KEYDOWN:
+            if e.key == pg.K_ESCAPE:
+                exit()
--- main.py
+++ main.py
@@ -6,8 +6,4 @@ sc = pg.display.set_mode((1920, 1080))
 
 
 while 1:
-    keys = pg.key.get_pressed()
-    if keys[pg.K_w]:
-        sys.exit()
-        print(1)
     maincontrol()
